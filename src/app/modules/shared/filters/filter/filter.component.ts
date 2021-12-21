cb96353c00f61a651cd99bf028a3d848792b4dc9
PINKALRAVAL <pinkal@ashutec.com>
2021-12-21 19:07:33 +0530

    Implement search field

--- src/app/modules/shared/filters/filter/filter.component.ts
+++ src/app/modules/shared/filters/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, Observable, OperatorFunction } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
@@ -9,11 +10,16 @@ export class FilterComponent implements OnInit {
   filterArray: string[] = ['All', 'test1', 'test2', 'test3', 'test4'];
   collapse = false;
   check = false;
+  searchArray: string[] = [];
   selectedFilters: string[] = ['All'];
   constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.searchArray = JSON.parse(JSON.stringify(this.filterArray));
+  }
+
   show() {
     this.collapse = !this.collapse;
+    this.searchArray = JSON.parse(JSON.stringify(this.filterArray));
   }
   onSelect(select: string) {
     if (this.selectedFilters.indexOf(select) != -1) {
@@ -36,5 +42,10 @@ export class FilterComponent implements OnInit {
   }
   onRemove(select: string) {
     this.onSelect(select);
+  }
+  onSearch(searchValue: any) {
+    this.searchArray = this.filterArray.filter((s) =>
+      s.toLocaleLowerCase().includes(searchValue.target.value)
+    );
   }
 }
