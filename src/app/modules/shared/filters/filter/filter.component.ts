eebe74f81c69ff21d1100423c8c0e6a259a04e40
PINKALRAVAL <pinkal@ashutec.com>
2021-12-21 16:30:09 +0530

    Implement collapse and expand button in filter component

--- src/app/modules/shared/filters/filter/filter.component.ts
+++ src/app/modules/shared/filters/filter/filter.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  filterArray: string[] = ['All', 'test1', 'test2', 'test3', 'test4'];
+  collapse = false;
+  check = false;
+  selectedFilters: string[] = ['All'];
+  constructor() {}
+  ngOnInit(): void {}
+  show() {
+    this.collapse = !this.collapse;
   }
-
+  onSelect(select: string) {
+    if (this.selectedFilters.indexOf(select) != -1) {
+      const i = this.selectedFilters.indexOf(select);
+      this.selectedFilters.splice(i, 1);
+      if (this.selectedFilters.length === 0) {
+        this.selectedFilters.push('All');
+      }
+    } else {
+      if (select === 'All') {
+        this.selectedFilters = ['All'];
+      } else {
+        if (this.selectedFilters.indexOf('All') != -1) {
+          const i = this.selectedFilters.indexOf('All');
+          this.selectedFilters.splice(i, 1);
+        }
+        this.selectedFilters.push(select);
+      }
+    }
+  }
+  onRemove(select: string) {
+    this.onSelect(select);
+  }
 }
